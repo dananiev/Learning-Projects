557e0611cfa2364edf3c1b63855f3265d4529b80
Deyan Ananiev <deyan.ananiev@gmail.com>
2020-05-29 12:07:13 +0300

    added projects

---
+++ CountdownTimer/script.js
@@ -0,0 +1,95 @@
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+const weekdays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+
+const weekday = weekdays[futureDate.getDay()];
+const date = futureDate.getDate();
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
+
+const futureTime = futureDate.getTime();
+// console.log(futureTime);
+
+function getRemaindingTime() {
+    const today = new Date().getTime();
+    // console.log(today);
+
+    // 1 sec = 1000ms;
+    // 1 min = 60sec;
+    // 1 hr = 60min;
+    // 1 d = 24hr;
+
+    const t = futureTime - today;
+    // console.log(t);
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneHour = 60 * 60 * 1000;
+    const oneMinute = 60 * 1000;
+
+    let days = t / oneDay;
+    days = Math.floor(days);
+    // console.log(days);
+
+    let hours = Math.floor((t % oneDay) / oneHour);
+    let minutes = Math.floor((t % oneHour) / oneMinute);
+    let seconds = Math.floor((t % oneMinute) / 1000);
+
+    const values = [days, hours, minutes, seconds];
+
+    function format(item) {
+        if (item < 10) {
+            return (item = `0${item}`);
+        }
+        return item;
+    }
+
+    items.forEach((item, index) => {
+        item.innerHTML = format(values[index]);
+    });
+
+    if (t < 0) {
+        clearInterval(countdown);
+        deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired!</h4>`;
+    }
+}
+
+let countdown = setInterval(getRemaindingTime, 1000);
+
+getRemaindingTime();
