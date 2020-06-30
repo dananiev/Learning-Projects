6ecce0f65f7d962a32fd6430c4ad06716e3de41b
Deyan Ananiev <deyan.ananiev@gmail.com>
2020-06-30 16:55:16 +0300

    added projects

---
+++ ResponsiveWebsiteOne/script.js
@@ -0,0 +1,9 @@
+const selectElement = (element) => document.querySelector(element);
+
+selectElement('.open').addEventListener('click', () => {
+    selectElement('.nav-list').classList.add('active');
+});
+
+selectElement('.close').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active');
+});
