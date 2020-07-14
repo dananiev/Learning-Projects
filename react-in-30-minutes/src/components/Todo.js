c2fc358ce2e9dab1a78d9121c52bdb7b556414a1
Deyan Ananiev <deyan.ananiev@gmail.com>
2020-07-14 10:52:51 +0300

    added projects

---
+++ react-in-30-minutes/src/components/Todo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Todo({ todo, toggleTodo }) {
+    function handleTodoClick() {
+        toggleTodo(todo.id);
+    }
+
+    return (
+        <div>
+            <input type='checkbox' onChange={handleTodoClick} checked={todo.complete} />
+            {todo.name}
+        </div>
+    )
+}
