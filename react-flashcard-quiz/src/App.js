4f0fb8ca1ef5c65bbaf99ec1b7a245ac3b7e71c5
Deyan Ananiev <deyan.ananiev@gmail.com>
2020-07-15 14:25:55 +0300

    added projects

---
+++ react-flashcard-quiz/src/App.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect, useRef } from 'react';
+import FlashcardList from './components/FlashcardList';
+import './App.css';
+import axios from 'axios'
+
+export default function App() {
+  const [flashcards, setFlashcards] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  const categoryEl = useRef();
+  const amountEl = useRef();
+
+  useEffect(() => {
+    axios.get('https://opentdb.com/api_category.php')
+      .then(res => {
+        setCategories(res.data.trivia_categories)
+      })
+  }, [])
+
+  useEffect(() => {
+    
+  }, [])
+
+  function decodeString(str) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    axios
+    .get('https://opentdb.com/api.php', {
+      params: {
+        amount: amountEl.current.value,
+        category: categoryEl.current.value
+      }
+    })
+    .then(res => {
+      setFlashcards(res.data.results.map((questionItem, index) => {
+        const answer = decodeString(questionItem.correct_answer);
+        const options = [...questionItem.incorrect_answers.map(a => decodeString(a)), answer]
+        return {
+          id: `${index}-${Date.now()}`,
+          question: decodeString(questionItem.question),
+          answer: answer,
+          options: options.sort(() => Math.random() -.5)
+        }
+      }))
+    })
+  }
+
+  return (
+    <div>
+    <form className='header' onSubmit={handleSubmit}>
+      <div className='form-group'>
+        <label htmlFor='category'>Category</label>
+        <select id='category' ref={categoryEl}>
+          {categories.map(category => {
+            return <option value={category.id} key={category.id}>{category.name}</option>
+          })}
+        </select>
+      </div>
+      <div className='form-group'>
+          <label htmlFor='amount'>Number Of Questions</label>
+          <input type='number' id='amount' min='1' step='1' defaultValue={10} ref={amountEl} />
+      </div>    
+      <div className='form-group'>
+          <button className='btn'>Generate</button>
+      </div>
+    </form>
+      <div className='container'>
+      <FlashcardList flashcards={flashcards} />
+      </div>
+    </div>
+  );
+}
+
+// const SAMPLE_FLASHCARDS = [
+//   {
+//     id: 1,
+//     question: 'Question 1',
+//     answer: 'Answer 1',
+//     options: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4']
+//   },
+//   {
+//     id: 2,
+//     question: 'Question 2',
+//     answer: 'Answer 2',
+//     options: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4']
+//   },
+//   // {
+//   //   id: 3,
+//   //   question: 'Question 3',
+//   //   answer: 'Answer 3',
+//   //   options: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4']
+//   // }
+// ]
