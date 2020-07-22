c4453f9488909c59cfc5b18a1ec1e32fe56d3df6
Deyan Ananiev <deyan.ananiev@gmail.com>
2020-07-22 11:56:10 +0300

    added projects

---
+++ MusicMaker/script.js
@@ -0,0 +1,21 @@
+function sequencer() {
+    const kick = new Tone.Player("./drum/kick-electro01.wav").toMaster();
+    const snare = new Tone.Player("./drum/snare-lofi02.wav").toMaster();
+    let index = 0;
+
+    Tone.Transport.scheduleRepeat(repeat, "8n");
+    Tone.Transport.start();
+
+    function repeat() {
+        let step = index % 8;
+        let kickInputs = document.querySelector(`.kick input:nth-child(${step + 1})`);
+        let snareInputs = document.querySelector(`.snare input:nth-child(${step + 1})`);
+
+        if (kickInputs.checked) kick.start();
+        if (snareInputs.checked) snare.start();
+
+        index++;
+    }
+}
+
+sequencer();
