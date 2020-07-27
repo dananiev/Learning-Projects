db2ce35faf40cadba2aa659254b8f75dbefdee11
Deyan Ananiev <deyan.ananiev@gmail.com>
2020-07-27 17:13:13 +0300

    added projects

---
+++ EcommerceWebsite/script.js
@@ -0,0 +1,14 @@
+const menuIcon = document.querySelector('.menu-icon')
+const menuItems = document.querySelector('#menu-items');
+
+menuItems.style.maxHeight = '0px';
+
+function menuToggle() {
+    if (menuItems.style.maxHeight == '0px') {
+        menuItems.style.maxHeight = '200px' 
+    } else {
+        menuItems.style.maxHeight = '0px';
+    }
+}
+
+menuIcon.addEventListener('click', menuToggle);
