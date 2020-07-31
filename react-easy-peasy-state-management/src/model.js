5789aee5acf3eff2c765a25175f2d3ef93cc824a
Deyan Ananiev <deyan.ananiev@gmail.com>
2020-07-31 19:13:38 +0300

    added projects

---
+++ react-easy-peasy-state-management/src/model.js
@@ -0,0 +1,33 @@
+import { thunk, action } from 'easy-peasy';
+import { v4 as uuidv4 } from "uuid";
+
+export default {
+    todos: [],
+    // Thunk&
+    fetchTodos: thunk(async actions => {
+        const res = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=5");
+        const todos = await res.json();
+
+        actions.setTodos(todos);
+    }),
+
+    // Actions
+    setTodos: action((state, todos) => {
+        state.todos = todos;
+    }),
+    add: action((state, todo) => {
+        todo.id = uuidv4();
+        state.todos = [...state.todos, todo];
+    }),
+    toggle: action((state, id) => {
+        state.todos.map(todo => {
+            if (todo.id === id) {
+                todo.completed = !todo.completed
+            }
+            return todo;
+        });
+    }),
+    remove: action((state, id) => {
+        state.todos = state.todos.filter(todo => todo.id !== id);
+    })
+};
