ee45be19fe3bc5fa450ce52f7460665de2431f3f
Deyan Ananiev <deyan.ananiev@gmail.com>
2020-07-31 15:18:00 +0300

    added projects

---
+++ react-image-finder/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Navbar from './components/navbar/Navbar';
+import Search from './components/search/Search';
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div>
+          <Navbar />
+          <Search />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+  
+}
+
+export default App;
