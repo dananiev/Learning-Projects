a6cc5c61b927f5680e3a053b9073b9faca520739
Deyan Ananiev <deyan.ananiev@gmail.com>
2020-08-02 12:38:49 +0300

    added projects

---
+++ react-router-for-beginners/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import About from './About';
+import Shop from './Shop';
+import Nav from './Nav';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/shop' component={Shop} />
+          <Route path='/shop/:id' />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+)
+
+export default App;
