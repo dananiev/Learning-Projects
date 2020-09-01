63794d69ae9868177891a6f742b268ee8ca601d4
Deyan Ananiev <deyan.ananiev@gmail.com>
2020-09-01 11:03:48 +0300

    added projects

---
+++ react-website-exercise/src/components/About.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h2>About</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Sed tincidunt id neque ac congue.
+                Nam pulvinar, enim nec consectetur congue, nibh erat viverra nunc, in mattis nunc tortor sollicitudin nulla.
+                Nulla non dui nisl. 
+                Nullam quis pretium est. 
+                Phasellus ac pretium sem. 
+                Mauris id porttitor erat. 
+                Vestibulum rhoncus ac dolor vitae tempus. 
+                Praesent eget nunc rhoncus, luctus arcu hendrerit, molestie risus. 
+                Mauris sollicitudin dui sed ligula posuere consequat. 
+                Fusce convallis nec diam ac egestas. 
+                Donec auctor fermentum euismod. 
+                Fusce venenatis neque dui, id ullamcorper ipsum congue in. 
+                Ut condimentum bibendum eros et pretium. 
+                Ut nec est viverra, semper mi non, venenatis lacus. 
+                Nulla scelerisque accumsan maximus. 
+                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Sed tincidunt id neque ac congue.
+                Nam pulvinar, enim nec consectetur congue, nibh erat viverra nunc, in mattis nunc tortor sollicitudin nulla.
+                Nulla non dui nisl. 
+                Nullam quis pretium est. 
+                Phasellus ac pretium sem. 
+                Mauris id porttitor erat. 
+                Vestibulum rhoncus ac dolor vitae tempus. 
+                Praesent eget nunc rhoncus, luctus arcu hendrerit, molestie risus. 
+                Mauris sollicitudin dui sed ligula posuere consequat. 
+                Fusce convallis nec diam ac egestas. 
+                Donec auctor fermentum euismod. 
+                Fusce venenatis neque dui, id ullamcorper ipsum congue in. 
+                Ut condimentum bibendum eros et pretium. 
+                Ut nec est viverra, semper mi non, venenatis lacus. 
+                Nulla scelerisque accumsan maximus. 
+                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+            </p>
+        </div>
+    )
+}
+
+export default About
