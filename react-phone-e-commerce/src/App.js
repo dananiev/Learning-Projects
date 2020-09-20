7ae17f9dbe40cd1e8602988b7830f3d804e58c7e
Deyan Ananiev <deyan.ananiev@gmail.com>
2020-09-20 17:40:38 +0300

    added projects

--- react-phone-e-commerce/src/App.js
+++ react-phone-e-commerce/src/App.js
@@ -4,9 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import ProductList from './components/ProductList';
 import Details from './components/Details';
-import Cart from './components/Cart';
+import Cart from './components/cart/Cart';
 import Default from './components/Default';
 import { Switch, Route } from 'react-router-dom';
+import Modal from './components/Modal';
 
 class App extends Component {
   render() {
@@ -19,6 +20,7 @@ class App extends Component {
           <Route path='/cart' component={Cart} />
           <Route component={Default} />
         </Switch>
+        <Modal />
       </React.Fragment>
     );
   }
