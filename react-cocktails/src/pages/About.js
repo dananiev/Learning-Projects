5cbb3657149688c760eff94ebd460c569ba7e5f9
Deyan Ananiev <deyan.ananiev@gmail.com>
2020-11-20 12:40:48 +0200

    added projects

---
+++ react-cocktails/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <section className='section about-section'>
+            <h1 className='section-title'>about us</h1>
+            <p>
+                Good Cocktail brings you pure cocktail mixers with natural botanical extracts & real fruit juice.
+                To us, it’s not just the fun of a cocktail that is important – it’s what does and doesn't go into it! 
+                These mixers are absolutely free from artificial colours, flavours and preservatives.
+                They are fun, have no nasty ingredients and are absolutely no fuss to make quickly 
+                when you are in the happy hour mood. 
+            </p>
+        </section>
+    )
+}
+
+export default About
